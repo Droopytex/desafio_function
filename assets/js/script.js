2895a81efe5765c6846da2f6fcbc2efd8fb58806
Droopytex <a.velasquezvega@gmail.com>
2024-03-16 00:02:38 -0300

    ejercicio pintar listo

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,9 @@
-function pintar() {
-  ele.style.backgroundColor = "yellow";
+function pintar(color = "green") {
+  ele.style.backgroundColor = color;
 }
-const ele = document.getElementById("ele1");
-ele.addEventListener("click", pintar);
+ele = document.getElementById("ele1");
+ele.addEventListener("click", function () {
+  pintar("yellow");
+});
+
+pintar();
